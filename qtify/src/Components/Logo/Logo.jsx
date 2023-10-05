2e2960e2707bc954fd220ee70a3e10872b3b46b0
rdssurya <rdssurya@gmail.com>
2023-10-05 19:02:06 +0530

    C3: Button

---
+++ qtify/src/Components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LogoImage from '../../Assets/Logo.png';
+
+const Logo = () => {
+  return (
+    <img src={LogoImage} alt='logo' style={{width: '67px'}}/>
+  )
+};
+
+export default Logo;
