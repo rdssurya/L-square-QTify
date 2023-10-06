989136f630aad1ce5090fa853fe973a7df58c8c5
rdssurya <rdssurya@gmail.com>
2023-10-06 21:26:18 +0530

    C4: Search Bar

--- qtify/src/Components/SearchBar/SearchBar.jsx
+++ qtify/src/Components/SearchBar/SearchBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+// import { ReactComponent as SearchIcon } from '../../Assets/SearchIcon.png';
 import SearchIcon from '../SearchIcon/SearchIcon';
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
   return (
-    <div className={styles.searchContent}>
-        <input className={styles.searchBar} type='search'placeholder='Search a album of your choice'/>
-        <button className={styles.searchButton}>{<SearchIcon/>}</button>
-    </div> 
+    <form className={styles.wrapper}>
+      <input className={styles.search} placeholder='Search a album of your choice'/>
+      <button className={styles.searchButton}>
+        <SearchIcon/>
+      </button>
+    </form>
   )
 };
 
